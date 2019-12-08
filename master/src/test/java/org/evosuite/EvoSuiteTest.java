c5793ccf174571c2e94a4e965597facc28e27f1f
Rotem <rotba@post.bgu.ac.il>
2019-12-08 14:46:53 +0200

    no intresting

--- master/src/test/java/org/evosuite/EvoSuiteTest.java
+++ master/src/test/java/org/evosuite/EvoSuiteTest.java
@@ -10,14 +10,14 @@ public class EvoSuiteTest {
     public void main() {
         EvoSuite.main(
                 new String[]{
-                        "-projectCP","C:\\Users\\amirelm\\Code\\rotem\\BugMiner\\test\\tested_projects\\MavenProj\\MavenProj\\sub_mod_1\\target\\classes;C:\\Users\\amirelm\\Code\\rotem\\BugMiner\\test\\tested_projects\\MavenProj\\MavenProj\\sub_mod_1\\target\\dependency",
+                        "-projectCP","C:\\Code\\Lab\\BugMiner\\test\\tested_projects\\MavenProj\\MavenProj\\sub_mod_1\\target\\classes;C:\\Code\\Lab\\BugMiner\\test\\tested_projects\\MavenProj\\MavenProj\\sub_mod_1\\target\\dependency",
                         "-class", "p_1.Amit",
                         "-Dassertion_strategy=ALL",
                         "-criterion",
                         "BRANCH:EXCEPTION:METHOD",
-                        "-Dtest_dir=C:\\Users\\amirelm\\Code\\rotem\\BugMiner\\test\\tested_projects\\MavenProj\\MavenProj\\sub_mod_1\\.evosuite\\best-tests",
+                        "-Dtest_dir=C:\\Code\\Lab\\BugMiner\\test\\tested_projects\\MavenProj\\MavenProj\\sub_mod_1\\.evosuite\\best-tests",
                         "-regressionSuite",
-                        "-Dregressioncp='..\\regression\\MavenProj\\sub_mod_1\\target\\classes;..\\regression\\MavenProj\\sub_mod_1\\target\\dependency'",
+                        "-Dregressioncp=C:\\Code\\Lab\\BugMiner\\test\\tested_projects\\MavenProj\\regression\\MavenProj\\sub_mod_1\\target\\classes;C:\\Code\\Lab\\BugMiner\\test\\tested_projects\\MavenProj\\regression\\MavenProj\\sub_mod_1\\target\\dependency",
                         "-Dregression_fitness=ALL_MEASURES",
                         "-seed=3333333",
                         "-Ddebug=true"
