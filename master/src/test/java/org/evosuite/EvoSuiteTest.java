58e1afbc2d1c45de35cc1a5b85b9666322375031
Rotem <rotba@post.bgu.ac.il>
2019-12-29 08:06:21 +0200

    nop

--- master/src/test/java/org/evosuite/EvoSuiteTest.java
+++ master/src/test/java/org/evosuite/EvoSuiteTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class EvoSuiteTest {
 
     @Test
-    public void main() {
+    public void main1() {
         EvoSuite.main(
                 new String[]{
                         "-projectCP","C:\\Code\\Lab\\BugMiner\\test\\tested_projects\\MavenProj\\MavenProj\\sub_mod_1\\target\\classes;C:\\Code\\Lab\\BugMiner\\test\\tested_projects\\MavenProj\\MavenProj\\sub_mod_1\\target\\dependency",
@@ -19,6 +19,21 @@ public class EvoSuiteTest {
                         "-regressionSuite",
                         "-Dregressioncp=C:\\Code\\Lab\\BugMiner\\test\\tested_projects\\MavenProj\\regression\\MavenProj\\sub_mod_1\\target\\classes;C:\\Code\\Lab\\BugMiner\\test\\tested_projects\\MavenProj\\regression\\MavenProj\\sub_mod_1\\target\\dependency",
                         "-Dregression_fitness=ALL_MEASURES",
+                        "-seed=3333333"
+                }
+        );
+    }
+
+    @Test
+    public void main2() {
+        EvoSuite.main(
+                new String[]{
+                        "-projectCP","C:\\Code\\Lab\\BugMiner\\test\\tested_projects\\MavenProj\\MavenProj\\sub_mod_1\\target\\classes;C:\\Code\\Lab\\BugMiner\\test\\tested_projects\\MavenProj\\MavenProj\\sub_mod_1\\target\\dependency",
+                        "-class", "p_1.Amit",
+                        "-Dassertion_strategy=ALL",
+                        "-criterion",
+                        "BRANCH:EXCEPTION:METHOD",
+                        "-Dtest_dir=C:\\Code\\Lab\\BugMiner\\test\\tested_projects\\MavenProj\\MavenProj\\sub_mod_1\\.evosuite\\best-tests",
                         "-seed=3333333",
                         "-Ddebug=true"
                 }
